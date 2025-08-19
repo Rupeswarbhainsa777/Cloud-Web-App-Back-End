6dd2f711c54e652f3b34fa461765eac5e8dd69ce
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2025-08-20 01:59:19 +0530

    first commit

---
+++ src/main/java/com/code/CloudShare/CloudShareApplication.java
@@ -0,0 +1,14 @@
+package com.code.CloudShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudShareApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(CloudShareApplication.class, args);
+	}
+
+}
